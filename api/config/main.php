1c230e10dde8246ca6cd1f4632c46203fa745046
ArtDinWin <andsmirastrav@mail.ru>
2020-07-16 12:34:50 +0300

    5 commit: api add

---
+++ api/config/main.php
@@ -0,0 +1,51 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'api\controllers',
+    'components' => [
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'response' => [
+            'class' => 'yii\web\Response',
+            'format' => 'json',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                'class' => 'yii\rest\UrlRule',
+                'controller' => 'posts',
+            ],
+        ],
+        
+    ],
+    'params' => $params,
+];
---
+++ api/controllers/PostsController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace api\controllers;
+use yii\helpers\ArrayHelper;
+use yii\rest\ActiveController;
+
+class PostsController extends ActiveController
+{
+
+    public $modelClass = 'api\models\Posts';
+
+}
